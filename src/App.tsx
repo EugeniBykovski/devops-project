64ee024ebe2705bed78bdf44899fa9565ca6aa97
EugeniBykovski <bykovskieugeni@mail.ru>
2023-08-21 17:30:27 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import { About } from "./pages/About";
+import { Welcome } from "./pages/Welcome";
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount((prev) => prev + 1);
+  const decrement = () => setCount((prev) => prev - 1);
+
+  return (
+    <div>
+      <h1>value = {count}</h1>
+      <button onClick={increment}>increment</button>
+      <button onClick={decrement}>decrement</button>
+
+      <div style={{ display: "flex", gap: 15 }}>
+        <Link to={"about"}>about</Link>
+        <Link to={"welcome"}>welcome</Link>
+      </div>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </div>
+  );
+};
---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,14 @@
+import { useSearchParams } from "react-router-dom";
+
+export const Welcome = () => {
+  let [searchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+
+  return (
+    <div>
+      <h1>id = {id}</h1>
+      <h1>Welcome PAGE</h1>
+    </div>
+  );
+};
